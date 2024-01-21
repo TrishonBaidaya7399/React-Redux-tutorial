35929547cf0962929014176dc09d8ac9592d62c3
TrishonBaidaya7399 <githubtrishon@gmail.com>
2024-01-21 20:13:48 +0600

    action, reducers, state ready!

--- react-redux/src/Counter/Counter.jsx
+++ react-redux/src/Counter/Counter.jsx
@@ -1,11 +1,17 @@
 const Counter = () => {
+    const handleIncrement=()=>{
+        
+    }
+    const handleDecrement=()=>{
+
+    }
     return (
         <div>
             <h2>Counter App</h2>
             <h3>Count: 0</h3>
             <div style={{display: "flex", gap: "10px", justifyContent: "center"}}>
-            <button>Increment</button>
-            <button>Decrement</button>
+            <button onClick={handleIncrement}>Increment</button>
+            <button disabled onClick={handleDecrement}>Decrement</button>
             </div>
         </div>
     );
